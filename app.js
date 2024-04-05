12ec357cf791ea5732e405d5bd0c8e623efcda94
Shiven-2007 <75038196+Shiven-2007@users.noreply.github.com>
2024-04-05 18:21:01 +0530

    somechanges

--- app.js
+++ app.js
@@ -31,7 +31,15 @@ button.addEventListener("click", async () => {
 });
 
 const animItem = document.getElementById("svg");
+let scale = Math.min((window.innerHeight/1080),(window.innerWidth/1920))
 
+window.addEventListener("resize",()=>{
+	scale = Math.min((window.innerHeight/1080),(window.innerWidth/1920));
+	root.style.setProperty('--scale', scale)
+})
+
+root = document.querySelector(":root")
+root.style.setProperty('--scale', scale )
 
 
 // const svgContainer = document.getElementById("svg");
@@ -81,14 +89,18 @@ async function loadAudio(src, id) {
 
 // loading assets
 async function startLoading() {
-	loadVideo("https://milosmusic.b-cdn.net/MainVideos/milo1.mp4", 1);
-	loadVideo("https://milosmusic.b-cdn.net/MainVideos/inter1.mp4", 2);
-	loadVideo("https://milosmusic.b-cdn.net/MainVideos/milo2branch1.mp4", 3);
-	loadVideo("https://milosmusic.b-cdn.net/MainVideos/inter2_1.mp4", 4);
-	loadVideo("https://milosmusic.b-cdn.net/MainVideos/inter2_2.mp4", 5);
-	loadVideo("https://milosmusic.b-cdn.net/MainVideos/inter2_3.mp4", 6);
-	loadAudio("https://milosmusic.b-cdn.net/MainVideos/1.mp3", 1);
+	loadVideo("./short/First.mp4", 1);
+	loadVideo("./videos/1.mp4", 2);
+	loadVideo("./short/Second_1.mp4", 3);
+	loadVideo("./videos/2_1.mp4", 4);
+	loadVideo("./videos/2_2.mp4", 5);
+	loadVideo("./videos/2_3.mp4", 6);
+	loadVideo("./videos/2_2_1080.mp4", 7);
+	loadAudio("./audio/1.mp3", 1);
+	loadVideo("./videos/3_1080.mp4", 8)
 }
+
+
 
 
 
@@ -139,7 +151,7 @@ videos[0].addEventListener("ended", async () => {
 	const option2 = buttons.appendChild(document.createElement("button"));
 	option1.classList.add("option");
 	option2.classList.add("option");
-	option1.innerHTML = "Tomorrows a new day, go to sleep";
+	option1.innerHTML = "Tomorrow's a new day, go to sleep";
 	option2.innerHTML = "Lemme check my Phone";
 	buttons.style.opacity = "1";
 	const endTimer = handleTimer(10, option1);
@@ -158,35 +170,66 @@ videos[0].addEventListener("ended", async () => {
 		document.getElementById("texture").style.opacity = "0.1";
 		maindiv.style.background = "#141817";
 	});
-	option2.addEventListener("click", () => {
-		console.log("clicked option 2");
+	option2.addEventListener("click", async () => {
+		endTimer();
+		videos[1].style.opacity = "0";
+		buttons.style.opacity = "0";
+		audios[0].pause();
+		await sleep(900);
+		while (buttons.firstChild) {
+			buttons.removeChild(buttons.firstChild);
+		}
+		videos[1].style.display = "none";
+		videos[6].style.opacity = "1";
+		videos[6].play()
+		maindiv.style.background = "#141817";
 	});
 });
 let iterCount = 0;
 videos[2].addEventListener("ended", () => snoozeInteraction());
+videos[6].addEventListener("ended", ()=>snoozeInteraction());
 let snoozeTimers=[];
 async function snoozeInteraction() {
 	const option1 = buttons.appendChild(document.createElement("button"));
 	const option2 = buttons.appendChild(document.createElement("button"));
 	option1.innerHTML = "Snooze";
 	option2.innerHTML = "Stop";
+	option2.addEventListener("mouseover", ()=>{
+		if (option2.disabled == true){
+			option2.innerHTML = "Nah, I'm gonna sleep more";
+		}
+		
+	})
+	option2.addEventListener("mouseleave",()=>{
+		option2.innerHTML = "Stop"
+	})
 	option1.classList.add("snoozestop");
 	option2.classList.add("snoozestop");
-	buttons.style.top = "calc(var(--scale)*340)";
+	buttons.style.top = "calc(var(--scale) * 335px)";
 	option2.disabled = true;
 	videos[2].style.opacity = "0";
+	videos[6].style.opacity = '0'
 	await sleep(1000);
 	buttons.style.opacity = "1";
 	videos[2].style.display = "none";
+	videos[6].style.display = 'none'
 	videos[3].style.opacity = "1";
 	videos[3].play();
 	videos[3].loop = true;
 	snoozeTimers.push(handleTimer(5, option1));
 	option1.addEventListener("click", () => secondInteraction(iterCount));
-	option2.addEventListener("click", () => {
-		console.log("clicked option 2");
+	option2.addEventListener("click", async () => {
+		snoozeTimers[snoozeTimers.length-1]();
+		videos[5].style.opacity="0"
+		videos[5].pause()
+		buttons.style.opacity = "0"
+		await sleep(1000)
+		buttons.style.display = 'none'
+		videos[7].style.opacity="1"
+		videos[7].play()
 	});
 }
+
 async function secondInteraction(i) {
 	snoozeTimers[0+i]();
 	videos[3 + i].style.opacity = "0";
