c20ce05b0417a16356921b480b94f67501427cd6
Shiven-2007 <75038196+Shiven-2007@users.noreply.github.com>
2024-04-07 04:17:59 +0530

    fixedstuff

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ loadedAssets = 0;
 
 const homepage = document.getElementById("homepage");
 const button = document.getElementById("startButton");
+
+
+
+window.mobileCheck = function() {
+	let check = false;
+	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	if (check){
+		document.getElementById("mobile").style.display="flex";
+		
+	}
+	
+  };
+window.mobileCheck()
 button.addEventListener("click", async () => {
 	startLoading();
 	animItem.play();
@@ -138,14 +151,14 @@ async function onLoaded() {
 }
 
 function handleTimer(time, func){
-	timer.style.display = "flex";
+	timer.style.opacity = "1"
 	timer.innerHTML = time;
 	const interval = setInterval(() => {
 		timer.innerHTML = time;
 		if (time == 0) {
 			clearInterval(interval);
 			func.click();
-			timer.style.display = "none";
+			timer.style.opacity = "0"
 		}
 		time--;
 	}, 1000)
@@ -162,13 +175,13 @@ function handleTimer(time, func){
 const buttons = document.getElementById("buttons");
 videos[0].addEventListener("ended", async () => {
 	videos[0].style.opacity = "0";
+	videos[1].loop = true;
+	audios[0].loop = true;
 	await sleep(900);
 	videos[0].style.display = "none";
 	videos[1].style.opacity = "1";
 	videos[1].play();
-	videos[1].loop = true;
 	audios[0].play();
-	audios[0].loop = true;
 	const option1 = buttons.appendChild(document.createElement("button"));
 	const option2 = buttons.appendChild(document.createElement("button"));
 	option1.classList.add("option");
@@ -176,7 +189,7 @@ videos[0].addEventListener("ended", async () => {
 	option1.innerHTML = "Tomorrow's a new day, go to sleep";
 	option2.innerHTML = "Lemme check my Phone";
 	buttons.style.opacity = "1";
-	const endTimer = handleTimer(10, option1);
+	const endTimer = handleTimer(100, option1);
 	option1.addEventListener("click", async () => {
 		endTimer();
 		videos[1].style.opacity = "0";
@@ -212,14 +225,15 @@ videos[0].addEventListener("ended", async () => {
 let iterCount = 0;
 videos[2].addEventListener("ended", () => snoozeInteraction());
 videos[6].addEventListener("ended", ()=>snoozeInteraction());
-let snoozeTimers=[];
+let snoozeTimers;
 async function snoozeInteraction() {
 	const option1 = buttons.appendChild(document.createElement("button"));
 	const option2 = buttons.appendChild(document.createElement("button"));
 	option1.innerHTML = "Snooze";
 	option2.innerHTML = "Stop";
-	option2.addEventListener("mouseover", ()=>{
+	option2.addEventListener("mouseover",async ()=>{
 		if (option2.disabled == true){
+			await sleep(50)
 			option2.innerHTML = "Nah, I'm gonna sleep more";
 		}
 		
@@ -240,37 +254,41 @@ async function snoozeInteraction() {
 	videos[3].style.opacity = "1";
 	videos[3].play();
 	videos[3].loop = true;
-	snoozeTimers.push(handleTimer(5, option1));
+	snoozeTimers = handleTimer(5, option1);
 	option1.addEventListener("click", () => secondInteraction(iterCount));
 	option2.addEventListener("click", async () => {
-		snoozeTimers[snoozeTimers.length-1]();
+		snoozeTimers();
 		videos[5].style.opacity="0"
 		videos[5].pause()
 		buttons.style.opacity = "0"
+		buttons.style.pointerEvents = "none"
 		await sleep(1000)
 		buttons.style.display = 'none'
+		buttons.style.pointerEvents = "all"
 		videos[7].style.opacity="1"
 		videos[7].play()
 	});
 }
 
 async function secondInteraction(i) {
-	snoozeTimers[0+i]();
+	snoozeTimers();
+
 	videos[3 + i].style.opacity = "0";
 	videos[3 + i].pause();
 	buttons.style.opacity = "0";
+	buttons.style.pointerEvents="none"
 	await sleep(1000);
 	if (i == 1) {
 		buttons.children[0].disabled = true;
 		buttons.children[1].disabled = false;
-		snoozeTimers.push(handleTimer(5, buttons.children[1]));
-
+		snoozeTimers = handleTimer(5, buttons.children[1]);
 	}else{
-		snoozeTimers.push(handleTimer(5, buttons.children[0]));
+		snoozeTimers = handleTimer(5, buttons.children[0]);
 	}
 	videos[3 + i].style.display = "none";
 	videos[4 + i].style.opacity = "1";
 	buttons.style.opacity = "1";
+	buttons.style.pointerEvents = "all"
 	videos[4 + i].loop = true;
 	videos[4 + i].play();
 	iterCount++;
